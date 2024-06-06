95a67053c8cbb00d621fc2a8c816269199d4bef7
Bryan King Pecho <bkpecho05@gmail.com>
2024-06-07 02:04:14 +0800

    fix: document doesn't have a <title> element / meta description

--- app.vue
+++ app.vue
@@ -1,4 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  title: "BK Bot",
+  meta: [
+    {
+      name: "description",
+      content:
+        "BK Bot is a conversational AI interface powered by Google's advanced language models.",
+    },
+  ],
+});
+</script>
 
 <template>
   <div>
