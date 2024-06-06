36034a079146e633ebe99191fedda99597609f2a
Bryan King Pecho <bkpecho05@gmail.com>
2024-06-07 03:11:01 +0800

    feat: store chatHistory temporarily in cookie

--- server/api/chat.ts
+++ server/api/chat.ts
@@ -1,4 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { parse, serialize } from "cookie";
 
 export default defineEventHandler(async (event) => {
   const api_key = useRuntimeConfig().api_key;
@@ -11,22 +12,42 @@ export default defineEventHandler(async (event) => {
   }
 
   const genAI = new GoogleGenerativeAI(api_key);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
   const query = getQuery(event);
   const msg = query.text;
 
+  const cookies = parse(event.req.headers.cookie || "");
+
+  //@ts-ignore
+  const chatHistory = JSON.parse(cookies.chatHistory || "[]");
+
+  // Start the chat with the history
   const chat = model.startChat({
-    history: [],
+    history: chatHistory,
     generationConfig: {
-      maxOutputTokens: 100,
+      maxOutputTokens: 500,
     },
   });
 
   //@ts-ignore
   const result = await chat.sendMessage(msg);
   const response = await result.response;
-  const text = response.text();
+  const returnText = response.text();
 
-  return text;
+  // Update chat history
+  chatHistory.push({ role: "user", parts: [{ text: msg }] });
+  chatHistory.push({ role: "model", parts: [{ text: returnText }] });
+
+  // Update cookies with new history
+  event.res.setHeader(
+    "Set-Cookie",
+    serialize("chatHistory", JSON.stringify(chatHistory), {
+      path: "/",
+      httpOnly: true,
+      maxAge: 60 * 5,
+    }),
+  );
+
+  return returnText;
 });
