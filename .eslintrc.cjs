e4e29c3be71df9b8722e951a3fe4f87de1a15729
Bryan King Pecho <bkpecho05@gmail.com>
2024-06-07 01:45:25 +0800

    chore: update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,7 @@
 module.exports = {
-	root: true,
-	extends: ["@nuxt/eslint-config", "plugin:prettier/recommended"],
+  root: true,
+  extends: ["@nuxt/eslint-config", "plugin:prettier/recommended"],
+  rules: {
+    "vue/multi-word-component-names": "off",
+  },
 };
