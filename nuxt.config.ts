193fa85baa111f156c352e269efc62c92e509632
Bryan King Pecho <bkpecho05@gmail.com>
2024-06-07 01:45:48 +0800

    chore: run eslint fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	runtimeConfig: {
-		api_key: process.env.NUXT_API_KEY,
-	},
+  runtimeConfig: {
+    api_key: process.env.NUXT_API_KEY,
+  },
 
-	devtools: { enabled: false },
-	modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+  devtools: { enabled: false },
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
 });
--- server/api/chat.ts
+++ server/api/chat.ts
@@ -1,32 +1,32 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export default defineEventHandler(async (event) => {
-	const api_key = useRuntimeConfig().api_key;
+  const api_key = useRuntimeConfig().api_key;
 
-	if (!api_key) {
-		throw createError({
-			statusCode: 500,
-			statusMessage: "Missing API key",
-		});
-	}
+  if (!api_key) {
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Missing API key",
+    });
+  }
 
-	const genAI = new GoogleGenerativeAI(api_key);
-	const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+  const genAI = new GoogleGenerativeAI(api_key);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
 
-	const query = getQuery(event);
-	const msg = query.text;
+  const query = getQuery(event);
+  const msg = query.text;
 
-	const chat = model.startChat({
-		history: [],
-		generationConfig: {
-			maxOutputTokens: 100,
-		},
-	});
+  const chat = model.startChat({
+    history: [],
+    generationConfig: {
+      maxOutputTokens: 100,
+    },
+  });
 
-	//@ts-ignore
-	const result = await chat.sendMessage(msg);
-	const response = await result.response;
-	const text = response.text();
+  //@ts-ignore
+  const result = await chat.sendMessage(msg);
+  const response = await result.response;
+  const text = response.text();
 
-	return text;
+  return text;
 });
