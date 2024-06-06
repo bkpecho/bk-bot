0cfc2de7830a9a0e4e58051494cc51ed591b9622
Bryan King Pecho <bkpecho05@gmail.com>
2024-06-07 02:02:20 +0800

    fix: <html> element does not have a [lang] attribute

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "en",
+      },
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+    },
+  },
+
   runtimeConfig: {
     api_key: process.env.NUXT_API_KEY,
   },
